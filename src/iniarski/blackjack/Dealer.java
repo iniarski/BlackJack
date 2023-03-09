60a98033576a0e4c4233d1faeeb71fa0ba62f89a
iniarski <filipmw2002@gmail.com>
2023-03-09 13:28:29 +0100

    Add Dealer class

---
+++ src/iniarski/blackjack/Dealer.java
@@ -0,0 +1,18 @@
+package iniarski.blackjack;
+
+public class Dealer extends Player{
+    @Override
+    public int play() {
+        // According to the rules of Blackjack if the dealer has score of less than 17 he has to hit
+        if (score < 17) {
+            return Player.HIT;
+        }
+        // else he has to stand
+        return Player.STAND;
+    }
+
+    // using Player() constructor
+    public Dealer(Card firstCard, Card secondCard) {
+        super(firstCard, secondCard);
+    }
+}
