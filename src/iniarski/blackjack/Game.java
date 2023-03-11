918ce03dcade13ff457b15bedc5f7a252bfa0cdf
iniarski <filipmw2002@gmail.com>
2023-03-11 19:15:44 +0100

    add start() and main()
    the program has now basic functionality

--- src/iniarski/blackjack/Game.java
+++ src/iniarski/blackjack/Game.java
@@ -1,13 +1,13 @@
 package iniarski.blackjack;
 
-import java.util.ArrayList;
-
 public class Game {
-    // players stored in an ArrayList
-    protected ArrayList<Player> players;
-
     // fields for holding game rules
     // TODO : write class for reading and storing game rules in .properties file from which the values will be taken
+    // values should be read in constructor
+
+    Game() {
+
+    }
 
     protected int nOfDecks = 8;
     protected int minBet = 10;
@@ -16,6 +16,56 @@ public class Game {
     protected int handsPlayed = 20;
 
     // TODO : write game logic
-    Game() {
+    void start() {
+        Dealer dealer = new Dealer();
+        ComputerPlayer player = new ComputerPlayer(startingMoney);
+
+        Deck deck = new Deck(nOfDecks);
+
+        for (int i = 0; i < handsPlayed; i++) {
+
+            System.out.println("Hand : " + (i + 1) + "\n");
+
+            System.out.println("Player : ");
+            player.setHand(deck.deal(), deck.deal());
+            player.printHand();
+
+            System.out.println("Dealer : ");
+            dealer.setHand(deck.deal());
+            dealer.printHand();
+
+            System.out.println("Player move");
+            int playerBet = player.bet(minBet, maxBet);
+            System.out.println("Player's bet : " + playerBet);
+            while (player.play() != 0 && player.getScore() <= 21) {
+                player.addCard(deck.deal());
+                player.printHand();
+            }
+
+            System.out.println("Dealer move");
+            dealer.addCard(deck.deal());
+            dealer.printHand();
+            while (dealer.play() != 0 && dealer.getScore() <= 21) {
+                dealer.addCard(deck.deal());
+                dealer.printHand();
+            }
+
+            // player win condition
+            if (dealer.getScore() > 21 || (player.getScore() <= 21 && player.getScore() > dealer.getScore())) {
+                System.out.println("Player wins");
+                player.winMoney(2 * playerBet);
+            } else {
+                System.out.println("The house wins");
+            }
+
+            System.out.println("Player's money : " + player.getMoney() + "\n");
+        }
+
+        System.out.println("Game over");
+    }
+
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.start();
     }
 }
