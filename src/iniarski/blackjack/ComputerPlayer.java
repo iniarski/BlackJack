66b59658550891157b96a583f79e1b09a6124486
iniarski <filipmw2002@gmail.com>
2023-03-11 09:38:12 +0100

    Write methods for handling money and betting

--- src/iniarski/blackjack/ComputerPlayer.java
+++ src/iniarski/blackjack/ComputerPlayer.java
@@ -2,10 +2,40 @@ package iniarski.blackjack;
 
 public class ComputerPlayer extends Player{
 
+
+    protected int money;
+    public ComputerPlayer(int money){
+        this.money = money;
+    }
+
     @Override
     public int play() {
         // TODO : Implement logic
         return Player.STAND;
     }
 
+    //
+    public int bet(int minBet, int maxBet) {
+        // TODO : Implement betting logic
+
+        int preferredBet = money / 20;
+
+        if (preferredBet < minBet) {
+            preferredBet = minBet;
+        } else if (preferredBet > maxBet) {
+            preferredBet = maxBet;
+        }
+
+        money -= preferredBet;
+
+        return preferredBet;
+    }
+
+    public void winMoney(int winnings) {
+        money += winnings;
+    }
+
+    public int getMoney() {
+        return money;
+    }
 }
