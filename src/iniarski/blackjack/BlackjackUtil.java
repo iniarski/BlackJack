7ae2eaa562d6c5d220e8cc187271904d962a01ea
iniarski <filipmw2002@gmail.com>
2023-03-12 19:17:35 +0100

    Add BlackjackUtil class

---
+++ src/iniarski/blackjack/BlackjackUtil.java
@@ -0,0 +1,33 @@
+package iniarski.blackjack;
+
+// class for handling miscellaneous tasks in the project
+
+public class BlackjackUtil {
+
+    private BlackjackUtil(){
+    }
+
+    public int calculateScore(int[] cardRanks) {
+
+        int score = 0;
+        boolean hasAce = false;
+
+        for(int n : cardRanks) {
+            if(n == 0) {
+                hasAce = true;
+            }
+
+            if (n < 10){
+                score += n + 1;
+            } else {
+                score += 10;
+            }
+        }
+
+        if(hasAce && score <= 11) {
+            score += 10;
+        }
+
+        return score;
+    }
+}
