39ebdffa0bcf2e5199206f0e09039bc0d7590cd0
iniarski <filipmw2002@gmail.com>
2023-03-13 15:13:59 +0100

    Add BlackjackUtil.calculateDealerChances

--- src/iniarski/blackjack/BlackjackUtil.java
+++ src/iniarski/blackjack/BlackjackUtil.java
@@ -2,9 +2,13 @@ package iniarski.blackjack;
 
 // class for handling miscellaneous tasks in the project
 
+import java.util.Arrays;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicReference;
+
 public class BlackjackUtil {
 
-    private static BlackjackUtil instance = new BlackjackUtil();
+    private static final BlackjackUtil instance = new BlackjackUtil();
     private BlackjackUtil(){
     }
 
@@ -13,6 +17,9 @@ public class BlackjackUtil {
     }
 
     public int calculateScore(int[] cardRanks) {
+
+        // method for calculating score
+        // works for both cards description in the form of int[10] and int[13]
 
         int score = 0;
         boolean hasAce = false;
@@ -34,5 +41,74 @@ public class BlackjackUtil {
         }
 
         return score;
+    }
+
+    public double calculateDealerChances(int[] dealerCards, int[] cardsInDeck, int scoreToBeat) {
+
+        int currentScore = calculateScore(dealerCards);
+
+        // if dealer has more than 1 card
+        if (dealerCards.length > 1) {
+            // check if score is equal or greater than 17 - condition for dealer to stand
+            if (currentScore >= 17) {
+                // in this case dealer stands
+                if (currentScore >= scoreToBeat && currentScore <= 21) { // dealer win condition
+                    return 1.0;
+                }
+                // else - dealer loses
+                return 0.0;
+            }
+        }
+
+        // This executes only if dealer has 1 card or is under 17 points
+
+        int numberOfCardsLeft = Arrays.stream(cardsInDeck).sum();
+
+        double[] cardProbabilities = new double[10];
+
+        for (int i = 0; i < 10; i++) {
+            cardProbabilities[i] = (double) cardsInDeck[i] / (double) numberOfCardsLeft;
+        }
+
+        // analyzing all possible cases recursively in parallel
+        CountDownLatch latch = new CountDownLatch(10);
+
+        AtomicReference<Double> atomicWinProb = new AtomicReference<>((double) 0);
+
+        for (int i = 0; i < 10; i++) {
+            int finalI = i;
+            Thread thread = new Thread(() -> {
+
+                // checking for edge case - if there are no cards of such rank left in deck
+
+                if (cardsInDeck[finalI] == 0) {
+                    latch.countDown();
+                    return;
+                }
+
+                int[] newDeck = cardsInDeck.clone();
+                newDeck[finalI]--;
+
+                int[] newHand = Arrays.copyOf(dealerCards, dealerCards.length + 1);
+                newHand[newHand.length - 1] = finalI;
+
+                // recursive call
+                atomicWinProb.set(calculateDealerChances(newHand, newDeck, scoreToBeat) * cardProbabilities[finalI] +
+                        atomicWinProb.get());
+
+                latch.countDown();
+            });
+
+            thread.start();
+        }
+
+        // waiting for threads to finish
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return atomicWinProb.get();
     }
 }
