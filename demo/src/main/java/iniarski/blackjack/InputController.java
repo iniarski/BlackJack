3434dd4a38fd83ba46275bdfcf11bbb99b2db434
piotr <gwardysp@gmail.com>
2023-04-10 19:20:36 +0200

    Created UI to get user inputs

--- demo/src/main/java/iniarski/blackjack/InputController.java
+++ demo/src/main/java/iniarski/blackjack/InputController.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import org.apache.logging.log4j.*;
 
 public class InputController {
 
@@ -25,8 +26,32 @@ public class InputController {
     @FXML
     private TextField tf5;
 
+    static final Logger LOGGER = LogManager.getLogger(InputController.class);
+
+    private short nOfDecks;
+    private int minBet;
+    private int maxBet;
+    private int startingMoney;
+    private int handsDealt;
+
     @FXML
     void handle(ActionEvent event) {
+
+        if (tf1.getText() == null || tf2.getText() == null || tf3.getText() == null || tf4.getText() == null
+                || tf5.getText() == null) {
+            // TODO: Create error message window
+        }
+        LOGGER.info("Button pressed");
+        nOfDecks = Short.parseShort(tf1.getText());
+        LOGGER.info("nOfDecks  = " + nOfDecks);
+        minBet = Integer.parseInt(tf2.getText());
+        LOGGER.info("minBet = " + minBet);
+        maxBet = Integer.parseInt(tf3.getText());
+        LOGGER.info("maxBet = " + maxBet);
+        startingMoney = Integer.parseInt(tf4.getText());
+        LOGGER.info("money = " + startingMoney);
+        handsDealt = Integer.parseInt(tf5.getText());
+        LOGGER.info("Hands = " + handsDealt);
 
     }
 
--- demo/src/main/java/module-info.java
+++ demo/src/main/java/module-info.java
@@ -1,7 +1,9 @@
 module iniarski.blackjack {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.apache.logging.log4j;
+    requires org.apache.logging.log4j.core;
 
-    opens iniarski.blackjack to javafx.fxml;
+    opens iniarski.blackjack to javafx.fxml, org.apache.logging.log4j, org.apache.logging.log4j.core;
     exports iniarski.blackjack;
 }
