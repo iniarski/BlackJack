66e1c42fd82f1e5391cee1c850b2bda60be64136
piotr <gwardysp@gmail.com>
2023-04-25 20:58:33 +0200

    CHARTS

--- demo/src/main/java/iniarski/blackjack/Simulation.java
+++ demo/src/main/java/iniarski/blackjack/Simulation.java
@@ -5,6 +5,15 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.stage.Stage;
+
+
+
 public class Simulation
 {
     protected int nOfSimulations;
@@ -49,11 +58,45 @@ public class Simulation
 
         System.out.println("Average result " + average);
 
+        Stage stage = new Stage();
+        final NumberAxis xAxis = new NumberAxis(0,handsPlayed,1);
+        final NumberAxis yAxis = new NumberAxis();
+        xAxis.setLabel("Hands PLayed");
+        yAxis.setLabel("Money");
+
+        LineChart lineChart = new LineChart<>(xAxis, yAxis);
+        lineChart.setPrefHeight(1080);
+        lineChart.setPrefWidth(1920);
+        
+        
+        for(int i = 0; i<nOfSimulations; i++)
+        {
+            XYChart.Series series = new XYChart.Series<>();
+            int iter = 0;
+            for(int j = 0; j<handsPlayed; j++)
+            {
+                series.getData().add(new XYChart.Data<>(iter,games[i].getMoneyHistogram()[j]));
+                iter++;
+            }
+            lineChart.getData().add(series);
+            
+        }
+
+        
+
+        Group root = new Group(lineChart);
+        Scene scene = new Scene(root,1920,1080);
+        stage.setScene(scene);
+        stage.show();
+
     }
 
     public static void main(String[] args)
     {
         Simulation sim = new Simulation();
         sim.simulate();
+
     }
+
+    
 }
